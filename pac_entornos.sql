275ea68d6b12d8f27a931044824b8740cbaff544
molinacode <vipexprexx@gmail.com>
2023-04-10 16:34:50 +0200

    update bbdd

--- pac_entornos.sql
+++ pac_entornos.sql
@@ -1,22 +1,22 @@
--- Creación de la tabla User
-CREATE TABLE User (
-    UserID int PRIMARY KEY AUTO_INCREMENT,
-    Name varchar(50) NOT NULL,
-    Email varchar(100) NOT NULL,
-    FullName varchar(100) NOT NULL,
-    Password varchar(255) NOT NULL,
-    Type varchar(20) NOT NULL,
-    Enabled bit NOT NULL,
-    AdminLevel int NOT NULL,
-    CONSTRAINT FK_User_Category FOREIGN KEY (AdminLevel) REFERENCES Category(CategoryID)
-);
-
 -- Creación de la tabla Category
 CREATE TABLE Category (
 CategoryID int PRIMARY KEY,
 Name varchar(50) NOT NULL
 );
 
+-- Creación de la tabla User
+CREATE TABLE User (
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    FullName VARCHAR(100) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Type VARCHAR(20) NOT NULL,
+    Enabled BIT NOT NULL,
+    AdminLevel INT NOT NULL,
+    CONSTRAINT FK_User_Category FOREIGN KEY (AdminLevel)
+        REFERENCES Category (CategoryID)
+);
 -- Creación de la tabla Product
 CREATE TABLE Product (
 ProductID int PRIMARY KEY,
